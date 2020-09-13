672a911fe02f63b5884dc12a52a97238ec270d5c
ksreeni <ksreeni@node0.cs744-grp4clstr.uwmadison744-f20-pg0.wisc.cloudlab.us>
2020-09-12 20:44:58 -0500

    adding spark script to sort given file (part 2)

---
+++ assignment1/part_2.py
@@ -0,0 +1,27 @@
+# make sure to set spark context etc up here (and imports)
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("TestApp").setMaster("spark://c220g5-111230vm-1.wisc.cloudlab.us:7077")
+sc = SparkContext(conf=conf)
+
+file_path = "hdfs://10.10.1.1:9000/assignment1/export.csv"
+
+# read the file from HDFS
+rdd_org = sc.textFile(file_path)
+# extract and filter out header
+header = rdd_org.first()
+rdd = rdd_org.filter(lambda line: line != header)
+
+rdd = rdd.map(lambda line: line.split(","))
+rdd = rdd.map(lambda line: (line[2] + "--" + line[-1], line))
+
+def print_nice(x):
+	for i in x:
+		print(i)
+
+
+rdd_sorted = rdd.sortByKey()
+
+rdd_sorted_file = rdd_sorted.map(lambda line: ",".join(line[1]))
+# write rdd_sorted_file to file
+rdd_sorted_file.saveAsTextFile("hdfs://10.10.1.1:9000/assignment1/sorted_output.csv")
