39c58f6e1ff1e09b00dfccb79e64ec2c6150eb09
kamikazekartik <sreenivasan.kartik@gmail.com>
2020-09-23 17:44:39 -0500

    testing scatter and gather behavior

---
+++ assignment2/distributed_training.py
@@ -0,0 +1,50 @@
+import os
+import torch
+import json
+import copy
+import numpy as np
+from torchvision import datasets, transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import logging
+import random
+import model as mdl
+import argparse
+
+device = "cpu"
+torch.set_num_threads(4)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='PyTorch Assignment')
+    parser.add_argument('--node-rank', type=int, default=0, metavar='N',
+                        help='rank-0 = Master')
+    parser.add_argument('--world-size', type=int, default=3, metavar='N',
+                        help='Number of nodes')
+
+
+    args = parser.parse_args()
+
+
+    os.environ['MASTER_ADDR'] = '10.10.1.1'
+    os.environ['MASTER_PORT'] = '29500'
+
+    if args.node_rank == 0:
+        # I'm the master!
+        x = torch.tensor([0., 0.])
+        tensor_list = [torch.zeros_like(x) for i in range(args.world_size)]
+
+        dist.gather(x, gather_list=tensor_list)
+        logger.info("Received the following tensors: {}".format(tensor_list))
+    else:
+        # I'm just another node
+        x = args.node_rank*torch.ones(2)
+        dist.gather(x, dst=0)
+        logger.info("Sent {} to master".format(x))
+
+    logger.info("EXITING")
+    return 0
+
+
