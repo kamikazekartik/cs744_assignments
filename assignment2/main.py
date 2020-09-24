506a8bc1f1a4bdee4e7b54af8650b0885248a54b
kamikazekartik <sreenivasan.kartik@gmail.com>
2020-09-23 21:48:56 -0500

    added gather and scatter for training

--- assignment2/main.py
+++ assignment2/main.py
@@ -10,6 +10,9 @@ import torch.optim as optim
 import logging
 import random
 import model as mdl
+import argparse
+import torch.distributed as dist
+
 device = "cpu"
 torch.set_num_threads(4)
 logger = logging.getLogger()
@@ -17,7 +20,15 @@ logging.basicConfig()
 logger.setLevel(logging.INFO)
 
 batch_size = 256 # batch for one node
-def train_model(model, train_loader, optimizer, criterion, epoch):
+
+# helper function because otherwise non-empty strings
+# evaluate as True
+def bool_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
+
+def train_model(model, train_loader, optimizer, criterion, epoch, args=None):
     """
     model (torch.nn.module): The model created to train
     train_loader (pytorch data loader): Training data loader
@@ -26,6 +37,7 @@ def train_model(model, train_loader, optimizer, criterion, epoch):
     epoch (int): Current epoch number
     """
 
+    model.train()
     # remember to exit the train loop at end of the epoch
     for batch_idx, (data, target) in enumerate(train_loader):
         # Your code goes here!
@@ -34,6 +46,28 @@ def train_model(model, train_loader, optimizer, criterion, epoch):
         output = model(data)
         loss = criterion(output, target)
         loss.backward()
+        w = list(model.parameters())
+        
+        if args.distributed:
+            for layer in range(len(w)):
+                grad_vec = w[i].grad.data
+            # need to do this per layer (#TODO: there might be a better way)
+                if args.node_rank == 0:
+                    # master node: gather remaining gradients
+                    tensor_list = [torch.zeros_like(grad_vec) for i in range(args.world_size)]
+                    dist.gather(grad_vec, gather_list=tensor_list)
+                    # logger.info("Received the following tensors: {}".format(tensor_list))
+                    mean_grad = torch.mean(torch.stack(tensor_list), dim=0)
+                    dist.scatter(mean_grad, [mean_grad for i in range(args.world_size)])
+                else:
+                    # worker node: send gradient
+                    dist.gather(grad_vec, dst=0)
+                    mean_grad = torch.zeros_like(grad_vec)
+                    # logger.info("Sent {} to master".format(x))
+                    dist.scatter(mean_grad, src=0)
+                logger.info("Finished computing mean gradient for layer {}".format(layer))
+                w[i].grad.data = mean_grad
+
         optimizer.step()
         
         if batch_idx % 20 == 0:
@@ -62,8 +96,26 @@ def test_model(model, test_loader, criterion):
             
 
 def main():
+    parser = argparse.ArgumentParser(description='PyTorch Assignment')
+    parser.add_argument('--distributed', type=bool_string, default=False,
+                        help='flag to run in distributed mode')
+    parser.add_argument('--node-rank', type=int, default=0, metavar='N',
+                        help='rank-0 = Master')
+    parser.add_argument('--world-size', type=int, default=4, metavar='N',
+                        help='Number of nodes')
+
+    args = parser.parse_args()
+
+    if args.distributed:
+        os.environ['MASTER_ADDR'] = '10.10.1.1'
+        os.environ['MASTER_PORT'] = '29500'
+        dist.init_process_group("gloo", rank=args.node_rank, world_size=args.world_size)
+        global batch_size
+        batch_size = batch_size/args.world_size
+
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
+
     transform_train = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
@@ -101,7 +153,7 @@ def main():
     test_model(model, test_loader, training_criterion)
     # running training for one epoch
     for epoch in range(1):
-        train_model(model, train_loader, optimizer, training_criterion, epoch)
+        train_model(model, train_loader, optimizer, training_criterion, epoch, args)
         test_model(model, test_loader, training_criterion)
 
 if __name__ == "__main__":
