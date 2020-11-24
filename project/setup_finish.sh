c870421854724fe03d2a23bf4301fb7072708965
tomreitz <tom@reitzinternet.com>
2020-11-24 16:41:25 -0600

    Create setup_finish.sh

---
+++ project/setup_finish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# install CUDA
+sudo apt install -y software-properties-common
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/ /"
+sudo add-apt-repository contrib
+sudo apt-get -y update
+sudo apt-get -y install cuda
+
+# check that NVIDIA drivers are present
+nvidia-smi
+
+# install pip
+sudo apt install -y python3-distutils
+sudo apt install -y python3-apt
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+
+# install required Python packages
+pip install numpy pandas matplotlib seaborn torch torchvision
+
+# allocate swap memory to prevent runtime errors
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# check swap memory is available
+free -h
+
+# now you should be able to put your Python code in main.py and run it with `python3 main.py`
