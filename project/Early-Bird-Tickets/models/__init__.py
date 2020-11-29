6d51a79a9fb8b1475a2fc63b38f8a48801a4e1d4
Matthew Crepea <mcrepea@stevens.edu>
2020-11-28 23:33:10 -0600

    Added first repo for Early Bird Tickets.

---
+++ project/Early-Bird-Tickets/models/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .vgg import *
+from .preresnet import *
+from .densenet import *
+from .channel_selection import *
+from .quantize import *
+from .wage_initializer import *
