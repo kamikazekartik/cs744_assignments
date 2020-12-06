7a650e2c264cb4c6c8bb07164cd9b02b7333fcfe
kamikazekartik <ksreenivasa2@wisc.edu>
2020-12-06 15:00:33 +0000

    pushing logs for resnet50 convergence test

--- project/run_main.sh
+++ project/run_main.sh
@@ -1,11 +1,12 @@
 python3 main.py --seed 42 \
+--max-epochs 150 \
 --lr 0.01 \
 --gamma 0.998 \
 --dataset Cifar10 \
 --model resnet50 \
 --batch-size 128 \
 --test-batch-size 1000 \
---use-amp True \
+--use-amp False \
 --use-half False \
 --preload-data False \
 --device=cuda \
@@ -15,4 +16,4 @@ python3 main.py --seed 42 \
 --nbits-grad=8 \
 --nbits-error=8 \
 --nbits-momentum=8 \
-> cifar10_resnet50_amp_log 2>&1
+> cifar10_resnet50_fullprec_convergence_log 2>&1
