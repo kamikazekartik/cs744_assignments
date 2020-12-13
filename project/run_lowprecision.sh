2a247e46766fca78e952442b0dbff46d1eb69e59
Tom Reitz <tom@reitzinternet.com>
2020-12-13 03:07:25 +0000

    adding run_lowprecision.sh

---
+++ project/run_lowprecision.sh
@@ -0,0 +1,20 @@
+python3 main.py --seed 42 \
+--max-epochs 161 \
+--lr 0.1 \
+--gamma 0.998 \
+--dataset Cifar10 \
+--model resnet50 \
+--batch-size 128 \
+--test-batch-size 1000 \
+--use-amp False \
+--use-half False \
+--preload-data False \
+--device=cuda \
+--low-prec=True \
+--nbits-weight=8 \
+--nbits-activate=8 \
+--nbits-grad=8 \
+--nbits-error=8 \
+--nbits-momentum=8 \
+--results-file=output/v100_fullmodel_2/results_resnet50_Cifar10_batchsize128_precision8bit.csv \
+> output/v100_fullmodel_2/log_resnet50_Cifar10_batchsize128_precision8bit 2>&1
